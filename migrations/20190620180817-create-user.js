118a52203d40b3f382964fb2f705a6ea65c28b08
temilaj <temi@temilajumoke.com>
2019-06-20 17:49:27 -0400

    include ip in user model

--- migrations/20190620180817-create-user.js
+++ migrations/20190620180817-create-user.js
@@ -11,6 +11,9 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
+      ipAddress: {
+        type: Sequelize.STRING
+      },
       birthDate: {
         type: Sequelize.DATE
       },
