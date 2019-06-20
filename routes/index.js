299f0b8fc17907c115f68aea2ae1a65f3f1cee38
temilaj <temi@temilajumoke.com>
2019-06-20 11:08:42 -0400

    update routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Smart Assist' });
 });
 
 router.get('/test', function(req, res) {
-  res.json({ title: 'Welcome to Express' });
+  res.json({ title: 'Test Smart Assist' });
+});
+
+router.get('/activity', function(req, res) {
+  const { body, query } = req;
+  console.log({body});
+  console.log({query});
+  const { temperature, heartRate } = req.query;
+  console.log({temperature, heartRate});
+  res.json({temperature, heartRate});
 });
 
 module.exports = router;
