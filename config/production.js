c14766c4a1104a978d1863b1f6b88c1fa0f2a20e
temilaj <temi@temilajumoke.com>
2019-06-20 14:48:18 -0400

    implement environment varibales

--- config/production.js
+++ config/production.js
@@ -3,4 +3,11 @@ module.exports = {
 	db: process.env.DATABASEURI,
 	port: process.env.PORT || 4000,
 	loglevel: process.env.LOGLEVEL,
+	database: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		databaseName: process.env.DB_NAME,
+		host: "127.0.0.1",
+		dialect: "mysql"
+	},
 };
--- models/index.js
+++ models/index.js
@@ -5,14 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config')
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config.database);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database.databaseName, config.database.username, config.database.password, config.database);
 }
 
 fs
